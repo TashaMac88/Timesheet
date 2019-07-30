22f8cc540b8b97e346b4697f41aac7e60ac42391
Kevin Buchik <kbuchik@gmail.com>
2019-07-30 12:36:55 -0700

    firebase config

--- app.js
+++ app.js
@@ -1,24 +1,53 @@
-$(".submit").on("click"){
+
+  var firebaseConfig = {
+    apiKey: "AIzaSyCDSe7YVyFCm3Uaop9ffgiPYMY-KDR1ul8",
+    authDomain: "timesheet-59f66.firebaseapp.com",
+    databaseURL: "https://timesheet-59f66.firebaseio.com",
+    projectId: "timesheet-59f66",
+    storageBucket: "",
+    messagingSenderId: "805998880268",
+    appId: "1:805998880268:web:3caca7f63ab47a6e"
+  }
+
+firebase.initializeApp(firebaseConfig);
+
+var database=firebase.database();
+//database.ref().on("value", function (event))
+
+
+$(".submit").on("click"   ){
 
 
 
-    var EmployeeName = ("<tr>") 
-    var role =("<tr>")
+
+
+
+    var EmployeeName = ("<tr>")
+    var role = ("<tr>")
     var startDate = ("<tr>")
     var monthlyRate = ("<tr>")
     var totalBilled = ("<tr>")
 
-   employeeName = $("#employee-name-input").val().trim();
-   role=$("#role-input").val().trim();
-   startDate=$("#start-date-input").val().trim();
-   monthlyRate=$("#monthly-rate-input").val().trim()
-
+    employeeName = $("#employee-name-input").val().trim();
+    role = $("#role-input").val().trim();
+    startDate = $("#start-date-input").val().trim();
+    monthlyRate = $("#monthly-rate-input").val().trim()
 
     $("#current-employees").append(EmployeeName)
-    $("#current-employees").append(role)
-    $("#current-employees").append(startDate)
-    $("#current-employees").append(monthlyRate)
+    $("#role").append(role)
+    $("#start-date").append(startDate)
+    $("#monthly-rate").append(monthlyRate)
+    $("#total-billed").append(totalBilled)
 
+
+
+
+    database.ref().set({
+       
+
+
+        
+      });
 
 
 
